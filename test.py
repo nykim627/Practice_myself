a83b086c9302323676708560803b781e33087ba2
Nayoung <hkny0627@gmail.com>
2022-10-20 13:09:48 +0900

    ossp2_branch

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 name = input("이름을 입력하세요: ")
 number = input("학번을 입력하세요: ")
 major = input("학과를 입력하세요: ")
+grade = input("학년을 입력하세요: ")
 
 print("\n<출력>\n이름: ",name,
-"\n학번: ",number, "\n학과: ",major)
+"\n학번: ",number, "\n학과: ",major,
+"\n학년: ",grade)
